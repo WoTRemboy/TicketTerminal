fda7caa249e74910fec040494ecc4de1cdf43444
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-05-02 13:32:54 +0300

    Initial Commit

---
+++ TicketTerminal/TicketTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketTerminalApp.swift
+//  TicketTerminal
+//
+//  Created by Roman Tverdokhleb on 02/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TicketTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
