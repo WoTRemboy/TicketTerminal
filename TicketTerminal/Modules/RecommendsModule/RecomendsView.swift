434b7b3463a4524279f370876ab37e3d64fbf10c
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-05-13 19:28:21 +0300

    empty recommends view added

---
+++ TicketTerminal/Modules/RecommendsModule/RecomendsView.swift
@@ -0,0 +1,18 @@
+//
+//  RecomendsView.swift
+//  TicketTerminal
+//
+//  Created by Roman Tverdokhleb on 13/05/2025.
+//
+
+import SwiftUI
+
+struct RecomendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecomendsView()
+}
